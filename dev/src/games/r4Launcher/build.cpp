dce6594ca7321c6bca5525b340947d83fe6da311
8fm <fm8f@ya.ru>
2024-08-22 18:00:50 +0300

    vs2022: fix error LNK2001: unresolved external symbol ___iob_func

--- dev/src/games/r4Launcher/build.cpp
+++ dev/src/games/r4Launcher/build.cpp
@@ -3,3 +3,12 @@
 */
 
 #include "build.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900)
+FILE _iob[] = { *stdin, *stdout, *stderr };
+
+extern "C" FILE* __cdecl __iob_func(void)
+{
+	return _iob;
+}
+#endif
