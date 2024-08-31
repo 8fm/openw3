7fa7358c36ff5ce5054e25fe3af4425e7a1b8922
8fm <fm8f@ya.ru>
2024-08-31 21:56:08 +0300

    GFx4: add Src/Kernel/HeapPT/HeapPT_SysAllocMMAP.h with single include

---
+++ dev/external/gfx4/Src/Kernel/HeapPT/HeapPT_SysAllocMMAP.h
@@ -0,0 +1,24 @@
+/**************************************************************************
+
+PublicHeader:   Kernel
+Filename    :   HeapPT_SysAllocMMAP.h
+Content     :   MMAP based System Allocator
+Created     :   2009
+Authors     :   Maxim Shemanarev
+
+Notes       :   System Allocator that uses regular malloc/free
+
+Copyright   :   Copyright 2011 Autodesk, Inc. All Rights reserved.
+
+Use of this software is subject to the terms of the Autodesk license
+agreement provided at the time of installation or download, or which
+otherwise accompanies this software in either electronic or hard copy form.
+
+**************************************************************************/
+
+#ifndef INC_SF_Kernel_SysAllocMMAP_H
+#define INC_SF_Kernel_SysAllocMMAP_H
+
+#include "../SF_SysAlloc.h"
+
+#endif
