ba165cb77ca1164c8d815ef21b8292da3bb6862c
8fm <fm8f@ya.ru>
2024-09-08 14:04:47 +0300

    GFx4: 1st version of CMake files for GFx4 Windows build

---
+++ dev/external/gfx4/Projects/cmake/3rdParty/BuildOptions.cmake
@@ -0,0 +1,29 @@
+# 3rdParty
+
+if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+    add_definitions(-DNDEBUG)
+else()
+    add_definitions(-D_DEBUG -DDEBUG -DPNG_DEBUG=1)
+endif()
+
+if (WIN32)
+    add_definitions(-DWIN32)
+endif()
+
+if (MSVC)
+    if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+        add_compile_options(/GS /Gy /Zc:wchar_t /Gm- /O2 /Ob1 /Zc:inline /fp:precise /errorReport:prompt /GF /WX- /Zc:forScope /GR- /Gd /MT /FC /nologo /Zl /diagnostics:column)
+    else()
+        add_compile_options(/GS /Zc:wchar_t /Z7 /Gm- /Od /Zc:inline /fp:precise /errorReport:prompt /WX- /Zc:forScope /RTC1 /GR- /Gd /MTd /FC /nologo /Zl /diagnostics:column)
+    endif()
+else()
+    add_compile_options(-std=c++14)
+
+    add_compile_options(-fPIC -ffast-math -finput-charset=UTF-8 -fshort-wchar)
+    add_compile_options(-ffunction-sections -fdata-sections)
+    add_compile_options(-mfpmath=sse -msse3)
+
+    # Verbose output
+    add_compile_options(-v)
+    add_link_options(-v)
+endif()
---
+++ dev/external/gfx4/Projects/cmake/SDK/BuildOptions.cmake
@@ -0,0 +1,46 @@
+# SDK
+
+add_definitions(-DSF_BUILD_STATICLIB -DCURL_STATICLIB -D_MBCS)
+if (${GAME_CONFIG} STREQUAL "Final")
+    add_definitions(-DSF_BUILD_SHIPPING)
+endif()
+if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+    add_definitions(-DNDEBUG)
+else()
+    add_definitions(-D_DEBUG)
+endif()
+
+if (WIN32)
+    add_definitions(-DWIN32 -D_WINDOWS)
+endif()
+
+if (MSVC)
+    if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+        add_compile_options(/MP /GS /W4 /Gy /Zc:wchar_t /Z7 /Gm- /Ox /Ob2 /Zc:inline /fp:precise /Zp8 /errorReport:prompt /GF /WX- /Zc:forScope /GR- /Gd /MT /FC /nologo /Zl /Ot /diagnostics:column)
+    else()
+        add_compile_options(/MP /GS /W4 /Zc:wchar_t /Z7 /Gm- /Od /Zc:inline /fp:precise /Zp8 /errorReport:prompt /WX- /Zc:forScope /RTC1 /GR- /Gd /MTd /FC /nologo /Zl /diagnostics:column)
+    endif()
+else()
+    add_compile_options(-std=c++14)
+
+    add_compile_options(-fPIC -ffast-math -finput-charset=UTF-8 -fshort-wchar)
+    add_compile_options(-ffunction-sections -fdata-sections)
+    add_compile_options(-mfpmath=sse -msse3)
+
+    # Verbose output
+    add_compile_options(-v)
+    add_link_options(-v)
+endif()
+
+add_library(${PROJECT_NAME} STATIC ${SRC_FILES})
+
+target_include_directories(${PROJECT_NAME} PRIVATE
+    "../../../../Include"
+    "../../../../Src"
+    "../../../../3rdParty/zlib-1.2.7"
+    "../../../../3rdParty/jpeg-8d"
+    "../../../../3rdParty/libpng-1.5.13"
+    "../../../../3rdParty/expat-2.1.0/lib"
+    "../../../../3rdParty/pcre"
+    "../../../../3rdParty/cri/pc/include"
+)
